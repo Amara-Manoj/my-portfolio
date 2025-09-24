fc5887ae38f1865d679be87f1a3c00915b5743e6
Amara Manoj Naga Sai Venkat <amaramanojnagasaivenkat@Amaras-MacBook-Air.local>
2025-09-25 02:14:29 +0530

    Name alignment with Image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,14 +17,14 @@ const Hero = () => {
           className="md:flex-1 max-w-2xl text-center md:text-left mb-12 md:mb-0"
         >
           <p className="text-accent font-mono text-lg">Hi, my name is</p>
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-lightest-slate mt-2 whitespace-nowrap">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-lightest-slate mt-2">
             Amara Manoj NagaSai Venkat
           </h1>
           <h2 className="text-3xl lg:text-5xl font-bold text-slate mt-2">
             I design and build reliable, scalable systems that power high-traffic applications.
           </h2>
           <p className="mt-6 text-slate mx-auto md:mx-0">
-            I’m a passionate software engineer with 2 years of experience specializing in backend development and site reliability engineering. My focus is on creating robust payment and telecom platforms that perform seamlessly under pressure. I’m dedicated to balancing feature innovation with performance optimization, observability, and incident response to ensure smooth and scalable operations.
+            I’m a passionate software engineer with 2 years of experience specializing in Application and Backend development. My focus is on creating robust payment and telecom Application/Solutions that perform seamlessly under pressure. I’m dedicated to balancing feature innovation with performance optimization, observability, and incident response to ensure smooth and scalable operations.
           </p>
           <div className="mt-8 flex items-center justify-center md:justify-start space-x-4">
             <Link
